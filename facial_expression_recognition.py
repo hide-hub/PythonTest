37730155c7b554ff685cdba34fea962483cfbbf2
hide-hub <o2onec3@gmail.com>
2019-11-27 20:03:39 +0900

    try to use LDA, but the result was bad because LDA is categorization for (basically) 2 classes

--- facial_expression_recognition.py
+++ facial_expression_recognition.py
@@ -6,6 +6,7 @@ import csv
 import numpy  as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from numpy      import matlib as mlib
 
 
 ##################################################
@@ -52,6 +53,61 @@ def read_dataset( csv_file_name ):
 # program body
 ##################################################
 
+X, Y, Usage = read_dataset( 'fer2013/fer2013.csv' )
+
+# normalize the value range X (0-255) to 0-1
+X = X / 255
+
+# try Linear Discriminant Analysis for reducing big data dimensions
+Xt = X[Usage==0,:]
+Yt = Y[Usage==0]
+
+
+# make within class scatter matrix
+wcsMatrix = np.zeros( (Xt.shape[1], Xt.shape[1]) )
+for cat in range( np.max( Yt ) + 1 ):
+    tmpX  = Xt[ Yt == cat ]
+    meanX = np.mean( tmpX, 0 )
+
+    tmpX = tmpX - mlib.repmat( meanX, tmpX.shape[0], 1 )
+    wcsMatrix += tmpX.T.dot( tmpX )
+
+# make between class scatter matrix
+bcsMatrix = np.zeros( (Xt.shape[1], Xt.shape[1]) )
+wholeMean = np.mean( Xt, 0 )
+for cat in range( np.max( Yt ) + 1 ):
+    tmpX  = Xt[ Yt == cat ]
+    meanX = np.mean( tmpX, 0 )
+
+    tmpX  = ( wholeMean - meanX ).reshape( len(meanX), 1 )
+    bcsMatrix += tmpX.dot( tmpX.T ) * np.sum( Xt==cat )
+
+
+# calculate eigen values and vectors
+eval, evec = np.linalg.eig( np.linalg.inv( wcsMatrix ).dot( bcsMatrix ) )
+
+# sort eigen values with corresponding eigen vectors
+pairs = [ ( np.abs(eval[i]), evec[:,i] ) for i in range( len(eval) ) ]
+pairs = sorted( pairs, key=lambda x: x[0], reverse=True )
+
+
+ax1 = evec[:,0].T.dot( Xt.T ).real
+ax2 = evec[:,1].T.dot( Xt.T ).real
+ax3 = evec[:,2].T.dot( Xt.T ).real
+
+# plt.scatter( ax1, ax2, c=Yt, cmap='rainbow', alpha=0.7, edgecolors='b' )
+# plt.show()
+# ↑全然だめだった。データがバラけることもない
+# いくら何でも軸 2 つは無理があったか
+
+# ということで 3 本目の軸をダメ元でやってみる
+from mpl_toolkits import mplot3d as Axis3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter3D( ax1[:100]*100, ax2[:100]*100, ax3[:100]*100, \
+            c=Yt[:100], cmap='rainbow', alpha=0.7, edgecolors='b' )
+plt.show()
+# 表示点数を 10 まで少なくしても表示されなかった (どこかに間違いがあるのか？)
 
 
 
